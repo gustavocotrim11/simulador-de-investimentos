7e0d2bd2c0e906af15ff41720ee54e506d45ca3a
Gustavo Cotrim <gustavocotrim11@gmail.com>
2022-02-07 16:03:36 -0300

    Installing Styled Components and creating global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <h1>Simulador de Investimentos</h1>
+    <>
+      <GlobalStyle />
+      <h1>Simulador de Investimentos</h1>
+    </>
   );
 }
